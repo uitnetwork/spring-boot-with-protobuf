d51f7102f1b5a707774ca7247050c9b72559f019
Ninh Doan <uitnetwork@gmail.com>
2018-03-07 00:45:04 +0800

    Init commit

---
+++ src/main/kotlin/com/uitnetwork/springbootwithprotobuf/SpringBootWithProtobufApplication.kt
@@ -0,0 +1,11 @@
+package com.uitnetwork.springbootwithprotobuf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootWithProtobufApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootWithProtobufApplication>(*args)
+}
---
+++ src/test/kotlin/com/uitnetwork/springbootwithprotobuf/SpringBootWithProtobufApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.uitnetwork.springbootwithprotobuf
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringBootWithProtobufApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
